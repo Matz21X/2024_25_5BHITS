a15e46749227fa1119be566e9230b3f2bb8168f9
maz <127680394+Matz21X@users.noreply.github.com>
2024-09-04 16:13:50 +0200

    Vault backup: 04/09/24 16:13

--- SEW-Prog/AlgoPerformance/src/main/java/at/htlhl/algoperformance/AlgoPerformance.java
+++ SEW-Prog/AlgoPerformance/src/main/java/at/htlhl/algoperformance/AlgoPerformance.java
@@ -1,4 +1,35 @@
 package at.htlhl.algoperformance;
 
 public class AlgoPerformance {
+
+    public static final int N = 8;
+
+    public AlgoPerformance() {
+        constantRuntime(N);
+    }
+
+    /**
+     * The code takes a constant amount of time to run.
+     * It's not dependent on the size of n.
+     *
+     * 0(1)
+     *
+     * @param n input
+     */
+    private void constantRuntime(int n) {
+        System.out.println("Constant Runtime 0(1)");
+        System.out.println("The input is n: " + n);
+        System.out.println();
+    }
+
+    private void logarithmRuntime(int n) {
+        System.out.println("Logarithm Runtime 0(1)");
+        System.out.println("The input is n: " + n);
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        new AlgoPerformance();
+    }
 }
