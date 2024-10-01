b0ec3e9cf9cc944c52041aada12ff4938d351fe7
maz <127680394+Matz21X@users.noreply.github.com>
2024-10-01 10:34:13 +0200

    Vault backup: 01/10/24 10:34

--- SEW-Prog/ObserverPatternDemo/src/main/java/at/htlhl/observerpatterndemo/ObserverPatternDemo.java
+++ SEW-Prog/ObserverPatternDemo/src/main/java/at/htlhl/observerpatterndemo/ObserverPatternDemo.java
@@ -1,4 +1,19 @@
 package at.htlhl.observerpatterndemo;
 
 public class ObserverPatternDemo {
+    public ObserverPatternDemo() {
+        ConsoleLineReader lineReader = new ConsoleLineReader();
+
+        LineLogger lineLogger = new LineLogger();
+        lineReader.addLineReadListener(lineLogger);
+
+        NetworkSender networkSender = new NetworkSender();
+        lineReader.addLineReadListener(networkSender);
+
+        lineReader.scanConsole();
+
+    }
+    public static void main(String[] args) {
+        new ObserverPatternDemo();
+    }
 }
