d28be2d31521a146b10b4066c64d9ce423996707
maz <127680394+Matz21X@users.noreply.github.com>
2024-10-08 10:39:51 +0200

    Vault backup: 08/10/24 10:39

--- SEW-Prog/GraphDemo/src/main/java/at/htlhl/graphdemo/GraphControl.java
+++ SEW-Prog/GraphDemo/src/main/java/at/htlhl/graphdemo/GraphControl.java
@@ -1,4 +1,37 @@
 package at.htlhl.graphdemo;
 
+import com.brunomnsilva.smartgraph.graph.DigraphEdgeList;
+import com.brunomnsilva.smartgraph.graph.Graph;
+
 public class GraphControl {
+    private Graph<String, String> graph;
+
+    public GraphControl() {
+        graph = new DigraphEdgeList<>();
+        buildGraph();
+    }
+
+    private void buildGraph() {
+        graph.insertVertex("A");
+        graph.insertVertex("B");
+        graph.insertVertex("C");
+        graph.insertVertex("D");
+        graph.insertVertex("E");
+        graph.insertVertex("F");
+        graph.insertVertex("G");
+
+        // Muss in beide Richtungen definiert werden
+        graph.insertEdge("A", "B", "AB");
+        graph.insertEdge("B", "A", "BA");
+        graph.insertEdge("A", "C", "AC");
+        graph.insertEdge("A", "D", "CA");
+        graph.insertEdge("A", "E", "AE");
+        graph.insertEdge("A", "F", "AF");
+        graph.insertEdge("A", "G", "AG");
+
+    }
+
+    public Graph<String, String> getGraph() {
+        return graph;
+    }
 }
---
+++ SEW-Prog/GraphDemo/src/main/java/at/htlhl/graphdemo/GraphView.java
@@ -0,0 +1,18 @@
+package at.htlhl.graphdemo;
+
+import com.brunomnsilva.smartgraph.containers.ContentZoomScrollPane;
+import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
+import javafx.scene.layout.BorderPane;
+
+public class GraphView extends BorderPane {
+
+    private SmartGraphPanel<String, String> smartGraphPanel;
+    private ContentZoomScrollPane contentZoomScrollPane;
+
+
+
+    public GraphView(GraphControl) {
+
+    }
+
+}
