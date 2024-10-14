6185304408fb635e5a5dfb9c93e730567aeeecb9
maz <127680394+Matz21X@users.noreply.github.com>
2024-10-14 10:06:55 +0200

    Vault backup: 14/10/24 10:06

---
+++ SEW-Prog/Ausprobieren/src/main/java/at/htlhl/Main.java
@@ -0,0 +1,10 @@
+package at.htlhl;
+
+public class Main {
+    public static void main(String[] args) {
+        int ichkanndasallesbaldnichtmehr = 10;
+        for (int i = 1; i <= Math.pow(2, ichkanndasallesbaldnichtmehr); i++){
+            System.out.println(i);
+        }
+    }
+}
