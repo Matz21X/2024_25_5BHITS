33db085aae41ab1fe3e9a6ce2d8d98bd915eb070
maz <matthi.hrbek@outlook.com>
2024-10-14 18:59:36 +0200

    Vault backup: 14/10/24 18:59 ASUS

--- SEW-Prog/Ausprobieren/src/main/java/at/htlhl/Main.java
+++ SEW-Prog/Ausprobieren/src/main/java/at/htlhl/Main.java
@@ -7,7 +7,23 @@ public class Main {
         ForscherFranz forscherFranz = new ForscherFranz();
         subject.addObserver(forscherFranz);
 
-        subject.scanBash();
-        
+        //subject.scanBash();
+
+
+        int bruh = 8;
+
+
+        for (int i = 1; i<=bruh; i= i*2){
+            System.out.println(i);
+        }
+
+        for (int i = 1; i<= Math.pow(2, bruh);i++){
+            System.out.println("Bruh:"+i);
+        }
+
+
+
+
+
     }
 }
