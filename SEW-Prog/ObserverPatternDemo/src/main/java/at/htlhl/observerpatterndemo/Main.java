2f18d070c6ce55f0ba277a3d3f9be940dbcd6c58
maz <matthi.hrbek@outlook.com>
2024-10-14 18:47:13 +0200

    Whoops

---
+++ SEW-Prog/ObserverPatternDemo/src/main/java/at/htlhl/observerpatterndemo/Main.java
@@ -0,0 +1,19 @@
+package at.htlhl.observerpatterndemo;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Subject subject = new Subject();
+
+        ForscherFranz forscherFranz = new ForscherFranz();
+        subject.addObserver(forscherFranz);
+
+        NetworkSender networkSender =  new NetworkSender();
+        subject.addObserver(networkSender);
+
+        LineLogger lineLogger = new LineLogger();
+        subject.addObserver(lineLogger);
+
+        subject.scanConsole();
+    }
+}
