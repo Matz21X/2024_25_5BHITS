9a644811989056681758602a0dba829245837b26
maz <127680394+Matz21X@users.noreply.github.com>
2024-10-21 14:24:56 +0200

    Vault backup: 21/10/24 14:24

---
+++ MEDT/ionic/blank/src/app/components/three-js-demo/three-js-demo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-three-js-demo',
+  templateUrl: './three-js-demo.component.html',
+  styleUrls: ['./three-js-demo.component.scss'],
+})
+export class ThreeJsDemoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
