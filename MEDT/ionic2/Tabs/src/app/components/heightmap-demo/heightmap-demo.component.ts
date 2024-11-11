1cdef51756ffcfa6e194de3eded054b6f9616b1e
maz <127680394+Matz21X@users.noreply.github.com>
2024-11-11 15:25:49 +0100

    Vault backup: 11/11/24 15:25

--- MEDT/ionic2/Tabs/src/app/components/heightmap-demo/heightmap-demo.component.ts
+++ MEDT/ionic2/Tabs/src/app/components/heightmap-demo/heightmap-demo.component.ts
@@ -34,13 +34,13 @@ export class HeightmapDemoComponent implements OnInit, AfterViewInit, OnDestroy 
   ngAfterViewInit(): void {
     this.scene = new Scene();
     this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.camera.position.set(0, 25, 25);
+    this.camera.position.set(0, 30, 90);
     this.camera.lookAt(0, 0, 0);
     this.renderer = new WebGLRenderer({canvas: this.canvasRef.nativeElement});
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
     const loader = new TextureLoader();
-    loader.load('assets/textures/heightmap.png', (texture: Texture) => this.onTextureLoaded(texture));
+    loader.load('assets/textures/heightmap3.png', (texture: Texture) => this.onTextureLoaded(texture));
 
     window.addEventListener('resize', this.onWindowResize.bind(this)); // Resize-Listener
 
