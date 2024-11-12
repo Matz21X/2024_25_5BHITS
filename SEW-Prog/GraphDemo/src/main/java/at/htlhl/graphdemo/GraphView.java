9e04d60150d3cf5231f909804c8a6f9a40135f33
maz <127680394+Matz21X@users.noreply.github.com>
2024-11-12 10:35:12 +0100

    Vault backup: 12/11/24 10:35

--- SEW-Prog/GraphDemo/src/main/java/at/htlhl/graphdemo/GraphView.java
+++ SEW-Prog/GraphDemo/src/main/java/at/htlhl/graphdemo/GraphView.java
@@ -5,17 +5,11 @@ import com.brunomnsilva.smartgraph.graphview.SmartCircularSortedPlacementStrateg
 import com.brunomnsilva.smartgraph.graphview.SmartGraphPanel;
 import com.brunomnsilva.smartgraph.graphview.SmartPlacementStrategy;
 import com.brunomnsilva.smartgraph.graphview.SmartStylableNode;
-import javafx.animation.PauseTransition;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.ToolBar;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.util.Duration;
 
 public class GraphView extends BorderPane {
     private SmartGraphPanel<String, String> smartGraphPanel;
@@ -31,55 +25,22 @@ public class GraphView extends BorderPane {
         testButton.setOnAction(new TestEventHandler());
         ToolBar toolBar = new ToolBar(testButton);
         setTop(toolBar);
-        addDoubleClickEventHandler();
-        addRightClickEventHandler();
+
+        
+
+
     }
 
     public void initAfterVisible() {
         smartGraphPanel.init();
     }
 
-    private void addDoubleClickEventHandler() {
-        smartGraphPanel.setVertexDoubleClickAction(vertex -> {
-            startColorSwitch(vertex);
-        });
-    }
 
-    private void addRightClickEventHandler() {
-        smartGraphPanel.getChildren().forEach(vertex -> {
-            vertex.setOnMouseClicked(event -> {
-                if (event.getButton() == MouseButton.SECONDARY) {
-                    ContextMenu contextMenu = new ContextMenu();
-                    MenuItem selectItem = new MenuItem("Select");
-                    MenuItem showDetailsItem = new MenuItem("Show Details");
-                    selectItem.setOnAction(e -> startColorSwitch((SmartStylableNode) vertex));
-                    showDetailsItem.setOnAction(e -> System.out.println("Vertex name: " + vertex));
-                    contextMenu.getItems().addAll(selectItem, showDetailsItem);
-                    contextMenu.show(vertex, event.getScreenX(), event.getScreenY());
-                }
-            });
-        });
-    }
-
-    private void startColorSwitch(SmartStylableNode vertex) {
-        vertex.setStyleClass("htlVertex");
-        PauseTransition pause = new PauseTransition(Duration.seconds(1));
-        pause.setOnFinished(event -> {
-            vertex.setStyleClass("htlVertex2");
-            PauseTransition repeatPause = new PauseTransition(Duration.seconds(1));
-            repeatPause.setOnFinished(e -> startColorSwitch(vertex));
-            repeatPause.play();
-        });
-        pause.play();
-    }
 
     private class TestEventHandler implements EventHandler<ActionEvent> {
         @Override
         public void handle(ActionEvent actionEvent) {
             SmartStylableNode vertex = smartGraphPanel.getStylableVertex("C");
-            if (vertex != null) {
-                startColorSwitch(vertex);
-            }
         }
     }
 }
